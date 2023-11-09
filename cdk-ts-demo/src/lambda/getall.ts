b71138fda15bec4abaa9211b0f45db006cd3f1bc
skrishna <saravananand.krishnamani@eonics.nl>
2023-11-09 20:49:00 +0100

    Localstack dynamodb connection issue resolved

--- cdk-ts-demo/src/lambda/getall.ts
+++ cdk-ts-demo/src/lambda/getall.ts
@@ -1,24 +1,15 @@
-// import * as AWS from 'aws-sdk';
-import {  DynamoDB, DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import * as AWS from 'aws-sdk';
+import {  DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import {
   DynamoDBDocumentClient,
   ScanCommand
 } from "@aws-sdk/lib-dynamodb";
 
+
 const client = new DynamoDBClient({
-    region: process.env.CDK_DEFAULT_REGION,
-    endpoint: 'http://localhost:4566',
-
-
+  region:process.env.CDK_DEFAULT_REGION,
+  endpoint: 'http://localhost.localstack.cloud:4566'
 });
-
-// const db = new DynamoDB({
-//     region: process.env.CDK_DEFAULT_REGION,
-//     endpoint: 'http://localhost:4566',
-//     accessKeyId: 'test',
-//     secretAccessKey: 'test'
-//   });
-
 const dynamo = DynamoDBDocumentClient.from(client);
 const TABLE_NAME = process.env.TABLE_NAME || '';
 
@@ -27,16 +18,11 @@ export const handler = async (event: any) => {
 
     try {
         console.log('connecting to dyname db: ' +  TABLE_NAME)
+
         var body = await dynamo.send(
-            new ScanCommand({ TableName: TABLE_NAME })
-          );
-
-        //   const params = {
-        //     TableName: TABLE_NAME
-        //   };
-
-        //   const response = await db.scan(params).promise();
-          
+          new ScanCommand({ TableName: TABLE_NAME })
+        );
+      
         const response = body?.Items;
         const formattedRes = response?JSON.stringify(response):'No value present';
         return { statusCode: 200, body: formattedRes };
