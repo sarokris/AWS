91bfc36d5309f8dd90454bc02ffd824de41f697e
skrishna <saravananand.krishnamani@eonics.nl>
2024-01-08 13:31:44 +0100

    initialized the springboot aws lambda example function

---
+++ springboot-aws-lambda/src/main/java/com/example/springbootawslambda/SpringbootAwsLambdaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootawslambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsLambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsLambdaApplication.class, args);
+	}
+
+}
